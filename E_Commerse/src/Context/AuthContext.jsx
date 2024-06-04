0e397a6e9a50223980ef291a3f808c30288f7ca3
Dhiraj Kene <kdprakash1712@gmail.com>
2024-06-04 15:25:27 +0530

    isAuthenticated state change to false

--- E_Commerse/src/Context/AuthContext.jsx
+++ E_Commerse/src/Context/AuthContext.jsx
@@ -7,7 +7,7 @@ export const AuthContext = createContext();//the value of createContext is store
 
 //function AuthContextProvider is used as wrpper componant to wrap the main App componant with the helps of this user can access the data anywhere in the project.
 export function AuthContextProvider({children}){
-    const[authDetails, setAuthDetails] = useState({ isAuthenticated: true, token: null, email: null })//this state is used to store the authstate status of user.
+    const[authDetails, setAuthDetails] = useState({ isAuthenticated: false, token: null, email: null })//this state is used to store the authstate status of user.
 
     const login = ({Token, email})=>{ //this login function is used to to store the Authdetails of user that is isAuthenticated, email and token(get from the reqres api),and also it changes the isAuthentication status true.
 
